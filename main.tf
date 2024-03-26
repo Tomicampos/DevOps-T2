b63189951c9df34ff82b0e0732264bcc88ffe5da
TomasCampos <tomicampos2233@gmail.com>
2024-03-26 08:55:07 -0300

    Se agregan reglas en main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,34 @@ provider "aws" {
 resource "aws_security_group" "instance_security_group" {
   name        = "instance_security_group"
   description = "Security group for EC2 instance"
+  ingress {
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+ingress {
+  from_port   = 3000
+  to_port     = 3000
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 }
 
 resource "aws_instance" "COMPRAS_dev_instance" {
   ami                    = "ami-0c101f26f147fa7fd"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance_security_group.id]
+  key_name = "vockey"
+
 
   tags = {
     Name = "ORG-COMPRAS-DEV"
